235a417292938d16b9f4c9f46bc879d733c1e93f
asoerensen <asoerensen@kpmg.com>
2024-08-19 17:35:26 +0200

    debug var

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,9 +6,9 @@ import * as os from 'os';
 async function main()
 {
     // npm run test --instanceName=Name --username=User --password=pass
-    console.log(process.env.npm_config_instanceName);
-    console.log(process.env.npm_config_username);
-    console.log(process.env.npm_config_password);
+    console.log(process.env.npm_config_instance);
+    console.log(process.env.npm_config_user);
+    console.log(process.env.npm_config_secret);
 
     const testWorkspace = fs.mkdtempSync(path.resolve(__dirname, os.tmpdir(), "testRun-"));
     console.log("workspace: " + testWorkspace);
@@ -22,18 +22,18 @@ async function main()
         // Passed to --extensionTestsPath
         const testRunnerPath = path.resolve(__dirname, './suite/');
 
-        if (!process.env.npm_config_instanceName || !process.env.npm_config_username || !process.env.npm_config_password)
+        if (!process.env.npm_config_instance || !process.env.npm_config_user || !process.env.npm_config_secret)
         {
-            throw new Error("Missing parameter: npm run test --instanceName=Name --username=User --password=password");
+            throw new Error("Missing parameter: npm run test --instance=Name --user=User --secret=password");
         }
 
         let options: TestOptions = {
             extensionDevelopmentPath: extensionPath,
             extensionTestsPath: testRunnerPath,
             extensionTestsEnv: {
-                instanceName: process.env.npm_config_instanceName,
-                userName: process.env.npm_config_username,
-                password: process.env.npm_config_password,
+                instanceName: process.env.npm_config_instance,
+                userName: process.env.npm_config_user,
+                password: process.env.npm_config_secret,
                 // workspaceName: path.basename(testWorkspace)
             },
             launchArgs: [testWorkspace]
